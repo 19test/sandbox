158392175b9e3ba2a4fc48d3cb61449dd749320b
Nurettin Şenyer <seyyah@bil.omu.edu.tr>
2016-05-21 08:55:28 +0300

    .

---
+++ so/deneysel/RotationFeatureMatchingExample.m
@@ -0,0 +1,50 @@
+% http://www.mathworks.com/help/images/examples/find-image-rotation-and-scale-using-automated-feature-matching.html
+% edit RotationFeatureMatchingExample
+original = imread('cameraman.tif');
+imshow(original);
+text(size(original,2),size(original,1)+15, ...
+    'Image courtesy of Massachusetts Institute of Technology', ...
+    'FontSize',7,'HorizontalAlignment','right');
+
+scale = 0.7;
+J = imresize(original, scale); % Try varying the scale factor.
+
+theta = 30;
+distorted = imrotate(J,theta); % Try varying the angle, theta.
+figure, imshow(distorted)
+
+ptsOriginal  = detectSURFFeatures(original);
+ptsDistorted = detectSURFFeatures(distorted);
+
+[featuresOriginal,   validPtsOriginal]  = extractFeatures(original,  ptsOriginal);
+[featuresDistorted, validPtsDistorted]  = extractFeatures(distorted, ptsDistorted);
+
+indexPairs = matchFeatures(featuresOriginal, featuresDistorted);
+
+
+matchedOriginal  = validPtsOriginal(indexPairs(:,1));
+matchedDistorted = validPtsDistorted(indexPairs(:,2));
+
+figure;
+showMatchedFeatures(original,distorted,matchedOriginal,matchedDistorted);
+title('Putatively matched points (including outliers)');
+
+[tform, inlierDistorted, inlierOriginal] = estimateGeometricTransform(...
+    matchedDistorted, matchedOriginal, 'similarity');
+
+figure;
+showMatchedFeatures(original,distorted, inlierOriginal, inlierDistorted);
+title('Matching points (inliers only)');
+legend('ptsOriginal','ptsDistorted');
+
+Tinv  = tform.invert.T;
+
+ss = Tinv(2,1);
+sc = Tinv(1,1);
+scale_recovered = sqrt(ss*ss + sc*sc)
+theta_recovered = atan2(ss,sc)*180/pi
+
+outputView = imref2d(size(original));
+recovered  = imwarp(distorted,tform,'OutputView',outputView);
+
+figure, imshowpair(original,recovered,'montage')
